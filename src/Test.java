ff91e3eba6a1fdbce5b12aff2552bdc05edc09d4
DDanjv <danjv.alt@gmail.com>
2024-09-04 17:28:17 -0400

    if statement

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
 	public static void main(String[] args) { 
     System.out.println("Hello World");
+    int x = 1;
+    if(x == 1){
+      System.out.println("working");
+    }
   }
 }
