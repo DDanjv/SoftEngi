6d9299b01ef9b14f4735a829c96f5248476050cc
DDanjv <danjv.alt@gmail.com>
2024-09-04 23:08:13 -0400

    remove if

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,5 @@
 public class Test {
 	public static void main(String[] args) { 
     System.out.println("Hello World");
-    int x = 1;
-    if(x == 1){
-      System.out.println("working");
-    }
   }
 }
