389f5b6a3c8e3ade270901c24a6c328be1a4958a
DDanjv <danjv.alt@gmail.com>
2024-09-05 12:33:12 -0400

    fix

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
 	public static void main(String[] args) { 
     System.out.println("Hello World");
+<<<<<<< Updated upstream
+=======
+    int  X= 1;
+    if(x == 1){
+      System.out.println("working");
+    }
+>>>>>>> Stashed changes
   }
 }
