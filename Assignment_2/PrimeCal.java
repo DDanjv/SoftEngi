60fc7f5b7070df15ecbeb460b9fef0a3bec156c8
Ivan Nunez <101413581+Ivannunez2803@users.noreply.github.com>
2024-09-16 10:37:49 -0400

    Add files via upload

---
+++ Assignment_2/PrimeCal.java
@@ -0,0 +1,14 @@
+public class PrimeCal {
+	public static void main(String[] args) {
+
+		int x = 39; //Sample value, replace with read value later
+
+		for(int i = 0; i < x; i++) {
+			if ((i % 2) != 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+}
