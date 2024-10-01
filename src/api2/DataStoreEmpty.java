6c52e4210c9ea57a419538577090012a6b38bf0f
ggrath10 <grathwohlgarrett@gmail.com>
2024-09-30 22:15:46 -0400

    Update DataStoreEmpty.java
    It may need to be changed a little bit based on the compute engine, but it should date in the computation list and store it

--- src/api2/DataStoreEmpty.java
+++ src/api2/DataStoreEmpty.java
@@ -1,20 +1,30 @@
 package api2;
 
-public class DataStoreEmpty implements DataStore{
-    private InputConfig Received;
-    private OutputConfig send;
+import java.util.ArrayList;
+import java.util.List;
 
-    public DataStoreEmpty (InputConfig Received){
-        this.Received = Received;
+public class DataStoreEmpty implements DataStore {
+    
+    private List<String> storedResults;  // A list to store results from ComputeEngine
+
+    // accepts Input configuration and initiales result store
+    public DataStoreEmpty(Input receivedInput) {
+        this.receivedInput = receivedInput;
+        this.storedResults = new ArrayList<>();
     }
+
     @Override
-    public Iterable<Integer> read(InputConfig input) {
-        throw new UnsupportedOperationException("cant read");
+    public Iterable<Integer> read(Input input) {
+        // This could be expanded, but for now throws an exception
+        throw new UnsupportedOperationException("Cannot read from DataStoreEmpty");
     }
+
     @Override
     public WriteResult appendSingleResult(OutputConfig output, String result) {
-        throw new UnsupportedOperationException("cant append'");
+        // Store the result from ComputeEngine
+        storedResults.add(result);
+        // return a successful WriteResult
+        return WriteResult.SUCCESS;
     }
-    
 
 }
