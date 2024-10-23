7803929d48e9aa9c0f3f46246e226ed16e4b2f82
ggrath10 <grathwohlgarrett@gmail.com>
2024-10-22 20:47:41 -0400

    Fixed up TestUser TODO's

--- test/TestUser.java
+++ test/TestUser.java
@@ -1,15 +1,16 @@
-package edu.softwareeng.sample;
 
 import java.io.File;
+
+import api2.ComputationCoordinatorEmpty;
 
 
 public class TestUser {
 	
 	// TODO 3: change the type of this variable to the name you're using for your
 	// User <-> ComputeEngine API; also update the parameter passed to the constructor
-	private final ComputationCoordinator coordinator;
+	private final ComputationCoordinatorEmpty coordinator;
 
-	public TestUser(ComputationCoordinator coordinator) {
+	public TestUser(ComputationCoordinatorEmpty coordinator) {
 		this.coordinator = coordinator;
 	}
 
@@ -19,6 +20,8 @@ public class TestUser {
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
+
+        coordinator.runComputeJob(inputPath, outputPath, delimiter);
 	}
 
 }
